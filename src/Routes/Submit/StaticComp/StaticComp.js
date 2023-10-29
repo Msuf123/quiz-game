f450632c3fc7e5e6cfeb32fca15814d4c04a30fe
Msuf123 <jack@gmil.com>
2023-10-29 15:49:07 +0530

    Done 60% of the progress

--- src/Routes/Submit/StaticComp/StaticComp.js
+++ src/Routes/Submit/StaticComp/StaticComp.js
@@ -1,7 +1,7 @@
 export default function StaticComp(){
     return(
         <div>
-            <h3>Result</h3>
+            <h3 style={{textAlign:'center',color:'rgb(233, 71, 98)',fontSize:'29px',fontWeight:710}}>Result!</h3>
         </div>
     )
 }
--- src/Routes/Submit/Submit.js
+++ src/Routes/Submit/Submit.js
@@ -1,11 +1,12 @@
 import Dynamic from "./Dynamic/Dynamic";
 import StaticComp from "./StaticComp/StaticComp";
-
+import style from './submit.module.css'
 export default function Submit(){
     return(
-        <div>
+        <div className={style.main}>
             <StaticComp></StaticComp>
             <Dynamic></Dynamic>
+            <button>Retake Exarm!</button>
         </div>
     )
 }
